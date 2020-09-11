b33b20f0a155188dbd13a9169515bcc15f37c3a5
maurya20 <mukesh.ice17@gmail.com>
2020-09-11 17:45:10 +0530

    consuming market blog data and producing a gallery using react

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,37 +1,41 @@
 import React from "react";
-import axios from "axios";
-
-
 
 class Blogs extends React.Component {
   state = {
-    todos: []
-  }
+    blogs: [],
+  };
   componentDidMount() {
-    fetch('http://127.0.0.1:8000/mapi/')
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({ todos: data })
-      console.log(this.state.todos)
-    })
-    .catch(console.log)
+    fetch("http://127.0.0.1:8000/mapi/")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ blogs: data });
+        console.log(this.state.blogs);
+      })
+      .catch(console.log);
   }
   render() {
-
     return (
-       <div className="container">
+      <div className="container">
         <h1>Trending Blogs </h1>
-        {this.state.todos.map((todo) => (
-  
-              <h5>{todo.heading}</h5>
-        
-        ))
-  }
-            
+        <div className="row">
+          {this.state.blogs.map((blog) => (
+            <div className="col-md-4">
+              <div className="thumbnail">
+                <img
+                  src={blog.pic}
+                  alt="Lights"
+                  style={{ width: "100%" }}
+                ></img>
+                <div className="caption">
+                  <p>{blog.heading}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
 }
 
 export default Blogs;
- 
