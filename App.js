e517ae48498fe7dda9c07837bbf6c4a8c3aec017
maurya20 <mukesh.ice17@gmail.com>
2020-10-28 16:41:46 +0530

    flex implemented

--- App.js
+++ App.js
@@ -1,29 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react'
-import { View, Text, Image } from "react-native";
+import React from 'react';
+import { Text, View,StyleSheet } from 'react-native';
 
-const App = () => {
-   return (
-     <View >
-        <Text style={{
-      height: 100,
-      padding: 30,
-      backgroundColor:"gray",
-      fontSize:30 }}>
-         I am the one who knoks!</Text>
-         <Image
-  style={{
-    width: 451,
-    height: 351,
-   //  resizeMode: 'contain',
-  }}
-  source={{
-    uri:'https://source.unsplash.com/random',
-  }}
-/>
-<Image style={{width:451,height:351}} source={{uri:'https://picsum.photos/200/300',
-}}/>
-</View>
-   )
+
+
+const styles = StyleSheet.create({
+   center: {
+     alignItems: 'center',
+     backgroundColor:'red',
+     marginTop: 40,
+     height:140,
+     flex:2
+   }
+ })
+const HelloWorldApp = () => {
+  return (
+    <View style={ styles.center }>
+      <Text > Hello, world!</Text>
+    </View>
+  )
 }
-export default App
+export default HelloWorldApp;
