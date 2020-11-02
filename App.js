4c9c738b075dd4ca06d84951705774828a51bc50
maurya20 <mukesh.ice17@gmail.com>
2020-11-02 11:49:17 +0530

    ScrollView implemented

--- App.js
+++ App.js
@@ -1,21 +1,93 @@
-import React, { useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import React from 'react';
+import { Image, ScrollView, Text } from 'react-native';
 
-const PizzaTranslator = () => {
-  const [text, setText] = useState('');
-  return (
-    <View style={{padding: 30}}>
-      <TextInput
-        style={{marginTop: 40,fontSize:30,borderBottomEndRadius:5}}
-        placeholder="Type here to translate!"
-        onChangeText={(e) => setText(e)}
-        defaultValue={text}
-      />
-      <Text style={{padding: 10, fontSize: 42}}>
-        {text}
-      </Text>
-    </View>
-  );
-}
+const logo = {
+  uri: 'https://source.unsplash.com/random',
+  width: 364,
+  height: 264
+};
 
-export default PizzaTranslator;
+export default App = () => (
+  <ScrollView style={{ paddingTop: 26 }}>
+    <Text style={{ fontSize: 26 }}>Your Galllary</Text>
+    <Image 
+    source={{uri: 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/sample_img.png'}} 
+    style={{width: 400, height: 400}} 
+/>
+    <Image source={logo} />
+    <Image 
+    source={{uri: 'https://picsum.photos/200/300'}} 
+    style={{width: 400, height: 400}} 
+/>
+<Image 
+    source={{uri: 'https://picsum.photos/200/301'}} 
+    style={{width: 400, height: 400}} 
+/>
+<Image 
+    source={{uri: 'https://picsum.photos/201/300'}} 
+    style={{width: 400, height: 400}} 
+/>
+    <Text style={{ fontSize: 96 }}>If you like</Text>
+    <Image 
+    source={{uri: 'https://picsum.photos/200/299'}} 
+    style={{width: 400, height: 400}} 
+/>
+<Image 
+    source={{uri: 'https://source.unsplash.com/random'}} 
+    style={{width: 400, height: 400}} 
+/>
+    <Image source={logo} />
+    <Text style={{ fontSize: 96 }}>Scrolling down</Text>
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Text style={{ fontSize: 96 }}>What's the best</Text>
+    <Image source={logo} />
+    <Image 
+    source={{uri: 'https://picsum.photos/200/300'}} 
+    style={{width: 400, height: 400}} 
+/>
+<Image 
+    source={{uri: 'https://picsum.photos/200/301'}} 
+    style={{width: 400, height: 400}} 
+/>
+<Image 
+    source={{uri: 'https://picsum.photos/201/300'}} 
+    style={{width: 400, height: 400}} 
+/>
+    <Text style={{ fontSize: 96 }}>If you like</Text>
+    <Image 
+    source={{uri: 'https://picsum.photos/200/299'}} 
+    style={{width: 400, height: 400}} 
+/>
+<Image 
+    source={{uri: 'https://source.unsplash.com/random'}} 
+    style={{width: 400, height: 400}} 
+/>
+    <Text style={{ fontSize: 96 }}>Framework around?</Text>
+    <Image 
+    source={{uri: 'https://picsum.photos/200/300'}} 
+    style={{width: 400, height: 400}} 
+/>
+<Image 
+    source={{uri: 'https://picsum.photos/200/301'}} 
+    style={{width: 400, height: 400}} 
+/>
+<Image 
+    source={{uri: 'https://picsum.photos/201/300'}} 
+    style={{width: 400, height: 400}} 
+/>
+    <Text style={{ fontSize: 96 }}>If you like</Text>
+    <Image 
+    source={{uri: 'https://picsum.photos/200/299'}} 
+    style={{width: 400, height: 400}} 
+/>
+<Image 
+    source={{uri: 'https://source.unsplash.com/random'}} 
+    style={{width: 400, height: 400}} 
+/>
+    <Text style={{ fontSize: 50,paddingBottom:30 }}>React Native</Text>
+  </ScrollView>
+);
