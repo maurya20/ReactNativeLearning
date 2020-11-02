3a39b6dfe959e325ff36f9b52afc60cd628a2419
maurya20 <mukesh.ice17@gmail.com>
2020-11-02 15:43:40 +0530

    Rendering posts data from jsonplaceholder

--- App.js
+++ App.js
@@ -1,93 +1,55 @@
-import React from 'react';
-import { Image, ScrollView, Text } from 'react-native';
+import React,{ useEffect, useState }  from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
-const logo = {
-  uri: 'https://source.unsplash.com/random',
-  width: 364,
-  height: 264
-};
 
-export default App = () => (
-  <ScrollView style={{ paddingTop: 26 }}>
-    <Text style={{ fontSize: 26 }}>Your Galllary</Text>
-    <Image 
-    source={{uri: 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/sample_img.png'}} 
-    style={{width: 400, height: 400}} 
-/>
-    <Image source={logo} />
-    <Image 
-    source={{uri: 'https://picsum.photos/200/300'}} 
-    style={{width: 400, height: 400}} 
-/>
-<Image 
-    source={{uri: 'https://picsum.photos/200/301'}} 
-    style={{width: 400, height: 400}} 
-/>
-<Image 
-    source={{uri: 'https://picsum.photos/201/300'}} 
-    style={{width: 400, height: 400}} 
-/>
-    <Text style={{ fontSize: 96 }}>If you like</Text>
-    <Image 
-    source={{uri: 'https://picsum.photos/200/299'}} 
-    style={{width: 400, height: 400}} 
-/>
-<Image 
-    source={{uri: 'https://source.unsplash.com/random'}} 
-    style={{width: 400, height: 400}} 
-/>
-    <Image source={logo} />
-    <Text style={{ fontSize: 96 }}>Scrolling down</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{ fontSize: 96 }}>What's the best</Text>
-    <Image source={logo} />
-    <Image 
-    source={{uri: 'https://picsum.photos/200/300'}} 
-    style={{width: 400, height: 400}} 
-/>
-<Image 
-    source={{uri: 'https://picsum.photos/200/301'}} 
-    style={{width: 400, height: 400}} 
-/>
-<Image 
-    source={{uri: 'https://picsum.photos/201/300'}} 
-    style={{width: 400, height: 400}} 
-/>
-    <Text style={{ fontSize: 96 }}>If you like</Text>
-    <Image 
-    source={{uri: 'https://picsum.photos/200/299'}} 
-    style={{width: 400, height: 400}} 
-/>
-<Image 
-    source={{uri: 'https://source.unsplash.com/random'}} 
-    style={{width: 400, height: 400}} 
-/>
-    <Text style={{ fontSize: 96 }}>Framework around?</Text>
-    <Image 
-    source={{uri: 'https://picsum.photos/200/300'}} 
-    style={{width: 400, height: 400}} 
-/>
-<Image 
-    source={{uri: 'https://picsum.photos/200/301'}} 
-    style={{width: 400, height: 400}} 
-/>
-<Image 
-    source={{uri: 'https://picsum.photos/201/300'}} 
-    style={{width: 400, height: 400}} 
-/>
-    <Text style={{ fontSize: 96 }}>If you like</Text>
-    <Image 
-    source={{uri: 'https://picsum.photos/200/299'}} 
-    style={{width: 400, height: 400}} 
-/>
-<Image 
-    source={{uri: 'https://source.unsplash.com/random'}} 
-    style={{width: 400, height: 400}} 
-/>
-    <Text style={{ fontSize: 50,paddingBottom:30 }}>React Native</Text>
-  </ScrollView>
-);
+
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={{color:"red",textAlign:"center",fontSize:60,backgroundColor:"yellow"}}>Posts App</Text>
+      <FlatList
+        data={data}
+        keyExtractor={({ id }) => id}
+        renderItem={({item}) => (
+        <View>
+        <Text style={styles.item}>❝ {item.title} ❞</Text>
+        <Text style={styles.postBody}>{item.body}</Text>
+        </View>
+        )}
+      />
+    </View>
+  );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+     flex: 1,
+     paddingTop: 22
+    },
+    item: {
+      padding: 10,
+      fontSize: 38,
+      color:"blue"
+    },
+  postBody:{
+    padding:10,
+    fontSize:20,
+    color:'#1B3D6C'
+  }
+  });
+  
+
+
+export default App
