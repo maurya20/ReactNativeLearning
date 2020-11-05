01f1a2b576edbbfcbda978ac8e973fea27c2f4cb
maurya20 <mukesh.ice17@gmail.com>
2020-11-05 11:15:07 +0530

    random color generator implemented using useState Hook

--- App.js
+++ App.js
@@ -1,61 +1,24 @@
-import React,{useState} from 'react';
-import { Text, View, Button} from 'react-native';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-// import { NavigationContainer } from '@react-navigation/native';
-// import AboutScreen from "./src/About"
-// import ProfileScreen from "./src/ProfileScreen"
-
-// function HomeScreen({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Button
-//         onPress={() => navigation.navigate('Notifications')}
-//         title="Go to notifications"
-//       />
-//     </View>
-//   );
-// }
-
-// function NotificationsScreen({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Button onPress={() => navigation.goBack()} title="Go back home" />
-//     </View>
-//   );
-// }
-
-// const Drawer = createDrawerNavigator();
-
-// export default function App() {
-//   return (
-//     <NavigationContainer >
-//       <Drawer.Navigator initialRouteName="Home">
-//         <Drawer.Screen name="Home" component={HomeScreen} />
-//         <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-//         <Drawer.Screen name="Profile" component={ProfileScreen} />
-//         <Drawer.Screen name="About" component={AboutScreen} />
-//       </Drawer.Navigator>
-//     </NavigationContainer>
-//   );
-// }
+import React,{useState} from 'react'
+import { View, Text,TouchableOpacity } from 'react-native'
 
 
-const TapCounter = ()=>{
 
-  const [counter, setCounter] = useState(66);
-
+export default function App() {
+  const [color, setColor] = useState([])
+  const colorGen = ()=>{
+    const red = Math.floor(Math.random()*256)
+    const green = Math.floor(Math.random()*256)
+    const blue = Math.floor(Math.random()*256)
+   
+    return `rgb(${red},${green},${blue})`
+  }
+  console.log(color)
   return (
     <View>
-      
-       <Text style={{padding:40, fontSize:50, color:'red'}}>{counter}</Text>
-      <Button title="Increase" onPress={()=>{
-        setCounter(counter + 1)}} />
-        <Text style={{padding:40, fontSize:50}}>My Tap Counter</Text>
-      <Button title="Decrease" onPress={()=>{
-        setCounter(counter - 1)}} />
+      <TouchableOpacity onPress={()=>setColor([...color,colorGen()])}>
+      <Text style={{marginTop:30, fontSize:40,color:"green"}}>Tap Screen </Text>
+      <View style={{width:400, height:600,backgroundColor:colorGen()}}/>
+      </TouchableOpacity>
     </View>
   )
 }
-
-
-export default TapCounter
